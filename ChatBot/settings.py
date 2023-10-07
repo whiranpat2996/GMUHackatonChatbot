4c83d145e290c992f65b7ccef07fa6e03f0e85f0
William <whiranpat2996@gmail.com>
2023-10-07 03:14:09 -0400

    Added Chat functionality

--- ChatBot/settings.py
+++ ChatBot/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Daphne
+ASGI_APPLICATION = "ChatBot.asgi.application"
